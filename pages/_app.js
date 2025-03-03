5f1ff864440f3aee618e1ba1bb60cdc91e429d10
Rayhan <rayhancse08@gmail.com>
2025-03-03 12:27:37 +0600

    add meta data

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
+
       <Layout>
+          <Head>
+              <meta charSet="UTF-8" />
+              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              <meta name="description" content="Author: In World Student Campus, In World Student Campus for information world all university" />
+              <title>In World Student Campus</title>
+          </Head>
         <Component {...pageProps} />
       </Layout>
   );
